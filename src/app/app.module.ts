39ea3e2413e34829134b1f847f85ad299ef07f6b
BoKeJoR <jorgecarmarquez@gmail.com>
2020-08-14 11:18:05 +0200

    Template con Servicios y Animaciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {HttpClientModule} from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
@@ -17,10 +19,12 @@ import { ItemComponent } from './pages/item/item.component';
     PortafolioComponent,
     AboutComponent,
     ItemComponent
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
